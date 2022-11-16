37bb186cb514e67398e205d5ef67ed3aea6d983f
Girish Salaskar <girishsalaskar812@gmail.com>
2022-11-16 17:25:04 +0530

    Skeleton added

---
+++ src/NewComp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class NewComp extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default NewComp;
